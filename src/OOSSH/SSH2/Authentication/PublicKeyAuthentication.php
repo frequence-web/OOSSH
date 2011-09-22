ea05514b3547b9d35a180a95853f5fdbab466099
yohang <yohan@giarelli.org>
2011-09-22 19:19:13 +0200

    Added passphrase to pubkey authentication

--- src/OOSSH/SSH2/Authentication/PublicKeyAuthentication.php
+++ src/OOSSH/SSH2/Authentication/PublicKeyAuthentication.php
@@ -7,22 +7,43 @@ use \OOSSH\Exception as Exception;
 
 class PublicKeyAuthentication implements AuthenticationInterface
 {
+    /**
+     * @var string
+     */
     private $pubkeyFile;
 
+    /**
+     * @var string
+     */
     private $privkeyFile;
 
+    /**
+     * @var string
+     */
     private $username;
 
-    function __construct($username, $pubkeyFile, $privkeyFile)
+    /**
+     * @var string
+     */
+    private $passphrase;
+
+    /**
+     * @param $username
+     * @param $pubkeyFile
+     * @param $privkeyFile
+     * @param null $passphrase
+     */
+    function __construct($username, $pubkeyFile, $privkeyFile, $passphrase = null)
     {
         $this->username = $username;
         $this->pubkeyFile = $pubkeyFile;
         $this->privkeyFile = $privkeyFile;
+        $this->passphrase = $passphrase;
     }
 
     public function authenticate($resource)
     {
-        if (!ssh2_auth_pubkey_file($resource, $this->username, $this->pubkeyFile, $this->privkeyFile)) {
+        if (!ssh2_auth_pubkey_file($resource, $this->username, $this->pubkeyFile, $this->privkeyFile, $this->passphrase)) {
             throw new Exception\AuthenticationFailed;
         }
     }
