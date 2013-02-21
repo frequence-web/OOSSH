53e579fc12981e0e5307aff9629826d366f87ed7
yohang <yohan@giarel.li>
2013-02-21 19:46:45 +0100

    Added factory methods to OOSSH class

--- src/OOSSH/OOSSH.php
+++ src/OOSSH/OOSSH.php
@@ -2,10 +2,88 @@
 
 namespace OOSSH;
 
+use OOSSH\Authentication\AuthenticationInterface;
+
 /**
  * @author Yohan GIARELLI <yohan@giarel.li>
  */
 class OOSSH
 {
     const VERSION = '0.1.0-dev';
+
+    /**
+     * @var ConnectionInterface[]
+     */
+    protected $connections;
+
+    /**
+     * @var AuthenticationInterface[]
+     */
+    protected $authentications;
+
+    /**
+     * @param string              $name
+     * @param ConnectionInterface $connection
+     *
+     * @return OOSSH
+     */
+    public function add($name, ConnectionInterface $connection)
+    {
+        $this->connections[$name] = $connection;
+
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @param bool   $autoConnect
+     * @param bool   $autoAuthenticate
+     *
+     * @return ConnectionInterface
+     */
+    public function get($name, $autoConnect = true, $autoAuthenticate = true)
+    {
+        $connection = $this->connections[$name];
+
+        if ($autoConnect && !$connection->isConnected()) {
+            $connection->connect();
+        }
+
+        if ($autoAuthenticate && !$connection->isAuthenticated()) {
+            $connection->authenticate($this->authentications[$name]);
+        }
+
+        return $connection;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function has($name)
+    {
+        return isset($this->connections[$name]);
+    }
+
+    /**
+     * @param string                  $name
+     * @param AuthenticationInterface $authentication
+     *
+     * @return OOSSH
+     */
+    public function setAuthentication($name, AuthenticationInterface $authentication)
+    {
+        $this->authentications[$name] = $authentication;
+
+        return $this;
+    }
+
+    /**
+     * @return OOSSH
+     */
+    public static function create()
+    {
+        return new self;
+    }
 }
